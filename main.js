6124f44dea07287b05d436c9bb3d9cbc4e1f51d3
kenuigabriel <kenui.bandeira@escola.pr.gov.br>
2025-06-10 10:31:51 -0300

    salvar

---
+++ main.js
@@ -0,0 +1,21 @@
+onst numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+
+function diminuiTamanho(){ 
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha
+}
+
+botoes[1].onclick = aumentaTamanho;
+
+function aumentaTamanho(){ 
+    tamanhoSenha = tamanhoSenha + 1;
+    numeroSenha.textContent = tamanhoSenha
+}
+
+console.log(botoes);
